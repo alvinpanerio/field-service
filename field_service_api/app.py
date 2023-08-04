ceb62c41cc7f337340f1cb73041f4f6c1bac1984
alvinpanerio <panerio.alvin18@gmail.com>
2023-08-04 14:11:23 +0800

    [ADD] Addsimple authentication to Odoo

--- field_service_api/app.py
+++ field_service_api/app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+
 import xmlrpc.client
 
 
 app = Flask(__name__)
+cors = CORS(app)
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/login", methods=["POST"])
+def log_in():
+    url = "https://sample-awb.odoo.com"
+
+    common = xmlrpc.client.ServerProxy("{}/xmlrpc/2/common".format(url))
+
+    common.version()
+
+    uid = common.authenticate(
+        "sample-awb", request.json.get("username"), request.json.get("password"), {}
+    )
+    if uid:
+        return "<p>Successful</p>"
+    else:
+        return "<p>not</p>"
